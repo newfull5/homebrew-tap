2ddd2ce872b110d602cb5644783d1ae01c4e44fc
SaeChan_Oh <dhtocks@naver.com>
2026-01-03 21:13:04 +0900

    Update pastesheet.rb

--- Casks/pastesheet.rb
+++ Casks/pastesheet.rb
@@ -2,10 +2,10 @@ cask "pastesheet" do
   version "0.1.0-beta"
   sha256 "c4d17023572ea09e436fa3285bd126c303b2267cbdd8f3cbfc5829ee02b49706"
 
-  url "https://github.com/saechan/PasteSheets/releases/download/v#{version}/PasteSheet_0.1.0_aarch64.dmg"
+  url "https://github.com/newfull5/PasteSheets/releases/download/v#{version}/PasteSheet_0.1.0_aarch64.dmg"
   name "PasteSheet"
   desc "Smart Clipboard Manager with Folder Organization"
-  homepage "https://github.com/saechan/PasteSheets"
+  homepage "https://github.com/newfull5/PasteSheets"
 
   app "PasteSheet.app"
 
