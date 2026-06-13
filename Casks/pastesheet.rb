7318c22acc525f6d971e3ea42e436eb9b353c45f
SaeChan_Oh <dhtocks@naver.com>
2026-06-14 02:27:20 +0900

    fix: update repo URL PasteSheets → PasteSheet

--- Casks/pastesheet.rb
+++ Casks/pastesheet.rb
@@ -2,10 +2,10 @@ cask "pastesheet" do
   version "0.3.0"
   sha256 "5eebc5ea86b0c7c33f0154d26ec6757865f85c0a86510d918dce65e51065b631"
 
-  url "https://github.com/newfull5/PasteSheets/releases/download/v#{version}/PasteSheet_#{version}_universal.dmg"
+  url "https://github.com/newfull5/PasteSheet/releases/download/v#{version}/PasteSheet_#{version}_universal.dmg"
   name "PasteSheet"
   desc "Smart Clipboard Manager with Folder Organization"
-  homepage "https://github.com/newfull5/PasteSheets"
+  homepage "https://github.com/newfull5/PasteSheet"
 
   app "PasteSheet.app"
 
